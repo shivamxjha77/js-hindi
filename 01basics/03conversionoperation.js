2f17fc8592ba86c9d4fa7be5299210ef6c6854fc
Shivam Jha <shivamxjha77@gmail.com>
2025-03-24 20:47:33 +0000

    conversion of data types

---
+++ 01basics/03conversionoperation.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueIntNumber = Number(score)
+//console.log(typeof valueIntNumber);
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
