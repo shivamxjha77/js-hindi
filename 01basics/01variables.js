2d12c6c9c3562a77fafc9cb68804a9bb1d9cc2f5
Shivam Jha <shivamxjha77@gmail.com>
2025-03-24 19:04:59 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,10 @@
+const accountID = 14434
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "223344"
+accounCity = "delhi"
+
+accountEmail = "shiv@.com"
+accountPassword = "42463"
+accounCity = "bangalore"
+
+console.table([accountEmail, accountID, accountPassword, accounCity])
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,4 @@
+ let state;
+ // number => 2 to power 53
+ // bigInt
+ // boolean => true/false
